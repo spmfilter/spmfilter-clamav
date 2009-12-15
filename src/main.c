b7c5ead06d97ddb5f54c367083dd9345623a4267
Axel Steiner <ast@space.net>
2009-12-15 14:24:40 +0100

    added virus notification, still buggy

--- src/main.c
+++ src/main.c
@@ -13,10 +13,16 @@
 #include <stdlib.h>
 #include <spmfilter.h>
 
+
 #define THIS_MODULE "clamav"
 
+#define SENDER_TOKEN "%sender%"
+#define RECIPIENT_TOKEN "%recipient%"
+#define VIRUS_TOKEN "%virus%"
+
+
 enum {
-	BUFSIZE = 512
+	BUFSIZE = 1024
 };
 
 typedef struct {
@@ -24,11 +30,15 @@ typedef struct {
 	int port;
 	int max_scan_size;
 	gboolean add_header;
-	gboolean send_report;
 	char *header_name;
+	int notification;
+	char *notification_template;
+	char *notification_sender;
 } CLAMAV_SETTINGS;
 
-int parse_clam_config(SETTINGS *settings, CLAMAV_SETTINGS *clam_settings) {
+CLAMAV_SETTINGS *clam_settings;
+
+int parse_clam_config(SETTINGS *settings) {
 	GError *error = NULL;
 	GKeyFile *keyfile;
 
@@ -50,9 +60,13 @@ int parse_clam_config(SETTINGS *settings, CLAMAV_SETTINGS *clam_settings) {
 	if (!clam_settings->max_scan_size)
 		clam_settings->max_scan_size = 5242880;
 	
-	clam_settings->send_report = g_key_file_get_boolean(keyfile,"clamav","send_report",NULL);
-	if (!clam_settings->send_report)
-		clam_settings->send_report = FALSE;
+	clam_settings->notification = g_key_file_get_integer(keyfile,"clamav","notification",NULL);
+	if (!clam_settings->notification)
+		clam_settings->notification = 0;
+		
+	clam_settings->notification_template = g_key_file_get_string(keyfile,"clamav","notification_template",NULL);
+	
+	clam_settings->notification_sender = g_key_file_get_string(keyfile,"clamav","notification_sender",NULL);
 	
 	clam_settings->add_header = g_key_file_get_boolean(keyfile,"clamav","add_header",NULL);
 	if (!clam_settings->add_header)
@@ -65,10 +79,111 @@ int parse_clam_config(SETTINGS *settings, CLAMAV_SETTINGS *clam_settings) {
 	TRACE(TRACE_DEBUG,"clam_settings->host: %s",clam_settings->host);
 	TRACE(TRACE_DEBUG,"clam_settings->port: %d",clam_settings->port);
 	TRACE(TRACE_DEBUG,"clam_settings->max_scan_size: %d",clam_settings->max_scan_size);
-	TRACE(TRACE_DEBUG,"clam_settings->send_report: %d",clam_settings->send_report);
+	TRACE(TRACE_DEBUG,"clam_settings->notification: %d",clam_settings->notification);
+	TRACE(TRACE_DEBUG,"clam_settings->notification_template: %s",clam_settings->notification_template);
+	TRACE(TRACE_DEBUG,"clam_settings->notification_sender: %s",clam_settings->notification_sender);
 	TRACE(TRACE_DEBUG,"clam_settings->add_header: %d",clam_settings->add_header);
 	TRACE(TRACE_DEBUG,"clam_settings->header_name: %s",clam_settings->header_name);
 	
+	return 0;
+}
+
+int get_template(char *template_file, char *sender, char *recipient, char *virus, char **content) {
+	int fh;
+	int count;
+	char buffer[512];
+	int pos = 0;
+	char *replace;
+	char *template;
+	int replace_size;
+	int token_size;
+	
+	fh = open(template_file,O_RDONLY);
+	
+	template = malloc(sizeof(char));
+	while((count = read(fh,buffer,512))) {
+		template = realloc(template,strlen(template) + count + 1);
+		strncat(template,buffer,count);
+	}
+	close(fh);
+
+	*content = malloc(strlen(template) + 1);
+
+	while(*template != '\0') {
+		if(*template == '%') {
+			if (strncmp(template,SENDER_TOKEN,strlen(SENDER_TOKEN)) == 0) {
+				replace = sender;
+				replace_size = strlen(sender);
+				token_size = strlen(SENDER_TOKEN);
+			}	else if (strncmp(template,RECIPIENT_TOKEN,strlen(RECIPIENT_TOKEN)) == 0) {
+				replace = recipient;
+				replace_size = strlen(recipient);
+				token_size = strlen(RECIPIENT_TOKEN);
+			} else if (strncmp(template,VIRUS_TOKEN,strlen(VIRUS_TOKEN)) == 0) {
+				replace = virus;
+				replace_size = strlen(virus);
+				token_size = strlen(VIRUS_TOKEN);
+			} else {
+				replace = NULL;
+			}
+			
+			if (replace != NULL) {
+				*content = realloc(*content,strlen(*content) + replace_size);
+				memcpy((*content + pos), replace, replace_size);
+				pos += replace_size;
+				template = template + token_size;
+			}
+		} 
+		(*content)[pos++] = *template++;
+	}
+	TRACE(TRACE_DEBUG,"CONTENT: %s",*content);
+	return 0;
+}
+
+int generate_message(char *content, char *recipient, char *nexthop) {
+	GIOChannel *fh;
+	GError *error = NULL;
+	MESSAGE *msg = NULL;
+
+	msg = g_slice_new(MESSAGE);
+	msg->rcpts = malloc(sizeof(char));
+	msg->rcpts[0] = g_strdup(recipient);
+	msg->num_rcpts = 1;
+	msg->from = g_strdup(clam_settings->notification_sender);
+	msg->message_file = g_strdup(gen_queue_file());
+	msg->nexthop = g_strdup(nexthop);
+
+	fh = g_io_channel_new_file(msg->message_file,"w",NULL);
+	if (g_io_channel_write_chars(fh,content,-1,NULL,&error) != G_IO_STATUS_NORMAL) 
+		TRACE(TRACE_ERR,"writing virus notification failed: %s", error->message);
+
+	g_io_channel_flush(fh,NULL);
+	g_io_channel_shutdown(fh,TRUE,NULL);
+	g_io_channel_unref(fh);
+	
+	smtp_delivery(msg);
+	
+	g_slice_free(MESSAGE,msg);
+}
+
+int send_notify(SETTINGS *settings, MAILCONN *mconn, char *virname) {
+	GIOChannel *fh;
+	char *buffer;
+	
+	char *addr;
+	int i;
+	
+	for (i=0; i < mconn->num_rcpts; i++) {
+		char *mail_content;
+		get_template(clam_settings->notification_template,
+			clam_settings->notification_sender,
+			mconn->rcpts[i]->addr,
+			virname,
+			&mail_content);
+		generate_message(mail_content,mconn->rcpts[i]->addr,settings->nexthop);
+		free(mail_content);
+	}
+
 	return 0;
 }
 
@@ -79,11 +194,11 @@ int load(SETTINGS *settings, MAILCONN *mconn) {
 	uint32_t conv;
 	char r_buf[BUFSIZE];
 	char *transmit;
-	CLAMAV_SETTINGS *clam_settings;
+	char *clam_result;
 	clam_settings = g_slice_new(CLAMAV_SETTINGS);
 
 	TRACE(TRACE_DEBUG,"clamav loaded");
-	if (parse_clam_config(settings,clam_settings)!=0) 
+	if (parse_clam_config(settings)!=0) 
 		return -1;
 
 	transmit = (char *)malloc((BUFSIZE + 4) * sizeof(char));
@@ -142,6 +257,7 @@ int load(SETTINGS *settings, MAILCONN *mconn) {
 
 	close(fh);
 
+	/* this is the final chunk, to terminate instream */
 	TRACE(TRACE_DEBUG,"file done, sending 0000 chunk");
 	transmit[0] = 0;
 	transmit[1] = 0;
@@ -159,7 +275,26 @@ int load(SETTINGS *settings, MAILCONN *mconn) {
 	ret = recv(fd_socket, r_buf, BUFSIZE, 0);
 	TRACE(TRACE_DEBUG,"got %d bytes back, message was: [%s]", ret, r_buf);
 	close(fd_socket);
+	clam_result = get_substring("^stream: (.*)(?!FOUND\b)\\b\\w+$",r_buf,1);
+	
+	/* virus detected? */
+	if (!MATCH(clam_result,"")) {
+		/* do we have to send a notification? */
+		if (clam_settings->notification == 1 | clam_settings->notification == 2) {
+			if (send_notify(settings, mconn, clam_result) != 0) 
+				TRACE(TRACE_WARNING,"failed to send notification mail");
+		}
+		TRACE(TRACE_DEBUG,"Virus found: %s", clam_result);
+	}
+	
+	/* need to add a header? */
+	if (clam_settings->add_header) {
+		if (add_header(mconn->queue_file,clam_settings->header_name,clam_result)!=0) {
+			TRACE(TRACE_ERR, "failed to add header");
+		}
+	}
 
+	g_free(clam_result);
 	g_free(transmit);
 	g_free(clam_settings->host); 
 	g_slice_free(CLAMAV_SETTINGS,clam_settings);
